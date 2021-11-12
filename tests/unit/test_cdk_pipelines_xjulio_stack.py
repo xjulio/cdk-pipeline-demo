76570c5ebe3bf1bef7cdae3db209d881f9d40579
Julio Damasceno <xjulio@gmail.com>
2021-11-12 03:36:06 +0000

    Initial commit

---
+++ tests/unit/test_cdk_pipelines_xjulio_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from cdk_pipelines_xjulio.cdk_pipelines_xjulio_stack import CdkPipelinesXjulioStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in cdk_pipelines_xjulio/cdk_pipelines_xjulio_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = CdkPipelinesXjulioStack(app, "cdk-pipelines-xjulio")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
